8b116c469ea2ad2f9dfe8b5dbbd9755a88ffc50e
najwa <najwashaharudinoj@gmail.com>
2024-05-14 16:01:44 +0800

    main screen drawer

--- lib/screens/user_panel/main_screen.dart
+++ lib/screens/user_panel/main_screen.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:uni_loot/utils/app_constant.dart';
-
-import '../auth_ui/welcome_screen.dart';
+import 'package:uni_loot/widgets/drawer_widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -18,25 +14,12 @@ class MainScreen extends StatelessWidget {
           statusBarColor: AppConstant.appSecondaryColor,
           statusBarIconBrightness: Brightness.light),
         backgroundColor: AppConstant.appMainColor,
-        title: Text(AppConstant.appMainName),
+        title: Text(
+            AppConstant.appMainName,
+          ),
         centerTitle: true,
-        actions: [
-          GestureDetector(
-            onTap: () async {
-              GoogleSignIn googleSignIn = GoogleSignIn();
-              FirebaseAuth _auth = FirebaseAuth.instance;
-              await _auth.signOut();
-              await googleSignIn.signOut();
-              Get.offAll(() => WelcomeScreen());
-            },
-            child:  const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.logout),
-            ),
-          )
-
-        ],
       ),
+      drawer: DrawerWidget(),
     );
   }
 }
