c67901f656bd4fc4b62f2e699343767cbca3432a
najwa <najwashaharudinoj@gmail.com>
2024-05-24 15:09:59 +0800

    add and decrease item in cart

--- lib/screens/user_panel/cart_screen.dart
+++ lib/screens/user_panel/cart_screen.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_swipe_action_cell/core/cell.dart';
 import 'package:get/get.dart';
 import 'package:uni_loot/models/cart_model.dart';
@@ -106,17 +108,48 @@ class _CartScreenState extends State<CartScreen> {
                               Text(cartModel.productTotalPrice.toString()),
                               SizedBox(width: Get.width / 20.0,
                               ),
-                              CircleAvatar(
-                                radius: 18.0,
-                                backgroundColor: AppConstant.appMainColor,
-                                child: Text('-'),
+                              GestureDetector(
+                                onTap: () async {
+                                  if(cartModel.productQuantity > 1){
+                                    await FirebaseFirestore.instance
+                                        .collection('cart')
+                                        .doc(user!.uid)
+                                        .collection('cartOrders')
+                                        .doc(cartModel.productId)
+                                        .update({
+                                      'productQuantity': cartModel.productQuantity -1,
+                                      'productTotalPrice': (double.parse(cartModel.fullPrice) * (cartModel.productQuantity - 1))
+                                    });
+                                  }
+                                },
+                                child: CircleAvatar(
+                                  radius: 14.0,
+                                  backgroundColor: AppConstant.appMainColor,
+                                  child: Text('-'),
+                                ),
                               ),
                               SizedBox(width: Get.width / 20.0,
                               ),
-                              CircleAvatar(
-                                radius: 18.0,
-                                backgroundColor: AppConstant.appMainColor,
-                                child: Text('+'),
+                              GestureDetector(
+                                onTap: () async {
+                                  if(cartModel.productQuantity > 0){
+                                    await FirebaseFirestore.instance
+                                        .collection('cart')
+                                        .doc(user!.uid)
+                                        .collection('cartOrders')
+                                        .doc(cartModel.productId)
+                                        .update({
+                                      'productQuantity': cartModel.productQuantity + 1,
+                                      'productTotalPrice':
+                                        double.parse(cartModel.fullPrice) + double.parse(cartModel.fullPrice) * (cartModel.productQuantity)
+                                    });
+                                  }
+                                },
+                                child: CircleAvatar(
+                                  radius: 14.0,
+                                  backgroundColor: AppConstant.appMainColor,
+                                  child: Text('+'),
+                                ),
                               )
                             ],
                           ),
