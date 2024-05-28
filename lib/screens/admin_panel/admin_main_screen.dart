d38a2e90dd24b0d0e14c3910456a37147215c533
najwa <najwashaharudinoj@gmail.com>
2024-05-28 21:39:34 +0800

    add admin drawer widget

--- lib/screens/admin_panel/admin_main_screen.dart
+++ lib/screens/admin_panel/admin_main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:uni_loot/utils/app_constant.dart';
+import 'package:uni_loot/widgets/admin_drawer_widget.dart';
+
+import '../../widgets/drawer_widget.dart';
 
 class AdminMainScreen extends StatefulWidget {
   const AdminMainScreen({super.key});
@@ -12,8 +16,12 @@ class _AdminMainScreenState extends State<AdminMainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppConstant.appMainColor,
+        centerTitle: true,
         title: Text("Admin Panel"),
       ),
+
+      drawer: AdminDrawerWidget(),
     );
   }
 }
---
+++ lib/widgets/admin_drawer_widget.dart
@@ -0,0 +1,117 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:uni_loot/screens/user_panel/cust_order_screen.dart';
+import 'package:uni_loot/utils/app_constant.dart';
+
+import '../screens/auth_ui/welcome_screen.dart';
+
+class AdminDrawerWidget extends StatefulWidget {
+  const AdminDrawerWidget({super.key});
+
+  @override
+  State<AdminDrawerWidget> createState() => _AdminDrawerWidgetState();
+}
+
+class _AdminDrawerWidgetState extends State<AdminDrawerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: Get.height/25),
+      child: Drawer(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20.0),
+            bottomRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Wrap(
+          runSpacing: 10,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Najwa"),
+                subtitle: Text("Version 1.0"),
+                leading: CircleAvatar(
+                  radius: 22.0,
+                  backgroundColor: AppConstant.appMainColor,
+                  child: Text("N"),
+                ),
+              ),
+            ),
+            Divider(
+              indent: 10.0,
+              endIndent: 10.0,
+              thickness: 1.5,
+              color: Colors.black,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Users"),
+                leading: Icon(Icons.production_quantity_limits),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Orders"),
+                leading: Icon(Icons.shopping_bag),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Item"),
+                leading: Icon(
+                    Icons.shopping_cart),
+                trailing: Icon(
+                    Icons.arrow_forward
+                ),
+                onTap: (){
+                  Get.back();
+                  Get.to(() => CustomerOrderScreen());
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListTile(
+                onTap: () async {
+
+                  GoogleSignIn googleSignIn = GoogleSignIn();
+                  FirebaseAuth _auth = FirebaseAuth.instance;
+                  await _auth.signOut();
+                  await googleSignIn.signOut();
+                  Get.offAll(() => WelcomeScreen());
+                },
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Logout"),
+                leading: Icon(Icons.logout),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: AppConstant.appSecondaryColor,
+      ),
+    );
+  }
+}
